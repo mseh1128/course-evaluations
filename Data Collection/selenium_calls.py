a0127bfd72d7057bd8956432b8a3427f98b31f43
Manav Sehgal <manavsehgal1128@gmail.com>
2018-05-14 17:23:26 -0400

    Changed webdriver to chrome and decreased execution time

--- Data Collection/selenium_calls.py
+++ Data Collection/selenium_calls.py
@@ -2,20 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
 import time
 
-# Create a new instance of the Firefox driver
+# Create a new instance of the Chrome driver
+start_time = time.time()
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+driver = webdriver.Chrome(chrome_options=options)
 
-options = Options()
-options.set_headless(headless=True)
-driver = webdriver.Firefox(firefox_options=options)
-# # go to the google home page
 driver.get("https://psns.cc.stonybrook.edu/psp/csprodg/EMPLOYEE/CAMP/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HC_CLASS_SEARCH_GBL&IsFolder=false&IgnoreParamTempl=FolderPath,IsFolder")
-driver.switch_to_frame("TargetContent")
+driver.switch_to.frame("TargetContent")
 
 subject = driver.find_element_by_css_selector('#SSR_CLSRCH_WRK_SUBJECT\$0')
-subject.send_keys("lds")
+subject.send_keys("wrt")
 
 course_number = driver.find_element_by_id("SSR_CLSRCH_WRK_CATALOG_NBR$1")
 course_number.send_keys("102")
@@ -26,26 +26,14 @@ login.click()
 
 i = 0
 professor_names = set()
-# while (True):
-#     try:
-#         instructor = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'win0divMTG_INSTR$' + str
-#                                                                                      (i))))
-#         professor_names.add(instructor.text)
-#         i += 1
-#     except Exception:
-#         # print(e)
-#         break
-
-# for i in professor_names:
-#     print(i)
-
-
-num_of_sections = driver.find_element_by_class_name('PSPAGECONTAINER')
-print(num_of_sections.innerHTML)
-
-
-# for i in range(1, 10):
-#     first_instuctor = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'win0divMTG_INSTR$' + str(i))))
-#     print(first_instuctor.text)
+instructor = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, 'win0divMTG_INSTR$' + str(i))))
+i += 1
+while True:
+    try:
+        instructor = driver.find_element_by_id('win0divMTG_INSTR$' + str(i))
+        professor_names.add(instructor.text)
+        i += 1
+    except NoSuchElementException:
+        break
 
 driver.quit()
