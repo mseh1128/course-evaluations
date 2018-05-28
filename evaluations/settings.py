8e02472f8e2ca18f7f6abc2b5e56f3b85572cf80
Manav Sehgal <manavsehgal1128@gmail.com>
2018-05-28 11:49:46 -0400

    Changed secret key for eventual production

--- evaluations/settings.py
+++ evaluations/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from .secret import secret_KEY
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qjgf_5rd*z)n^jkhekob$_6g=81y3jx1i__$gk&5uh^yj$_*m4'
+SECRET_KEY = secret_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
